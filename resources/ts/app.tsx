0eb530812aaa9f997b1fd2129b97fe2fabef255c
rdzPedraos <rdzpedraos@gmail.com>
2024-09-11 20:59:06 -0500

    refactor: execute npm run format for format js code

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,16 +1,16 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
 
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import {createInertiaApp } from '@inertiajs/react'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
 
 createInertiaApp({
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.tsx');
-    return pages[`./Pages/${name}.tsx`]()
-  },
-  setup({ el, App, props }) {
-    createRoot(el).render( <App {...props} /> )
-  },
-})
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.tsx");
+        return pages[`./Pages/${name}.tsx`]();
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+});

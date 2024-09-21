76265cedf1f36d648d0c201146e409433a720394
rdzPedraos <rdzpedraos@gmail.com>
2024-09-21 18:45:05 -0500

    Refactor: Remove session token regeneration in logout
    The session token regeneration code was commented out in the logout function of the LoginController. This code is no longer needed and has been removed.
    
    Refactor: Remove session token regeneration in logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -46,7 +46,7 @@ class LoginController extends Controller
     public function destroy(Request $request)
     {
         Auth::logout();
-        $request->session()->invalidate();
+        #$request->session()->invalidate();
         #$request->session()->regenerateToken();
 
         return redirect()->route("login");

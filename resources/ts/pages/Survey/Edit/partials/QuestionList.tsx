6be5ec84ac925c8d03001371936f27b9a43825cf
rdzPedraos <rdzpedraos@gmail.com>
2024-09-30 17:10:40 -0500

    feat: add handleClickoutside

--- resources/ts/pages/Survey/Edit/partials/QuestionList.tsx
+++ resources/ts/pages/Survey/Edit/partials/QuestionList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useFormCreateContext } from "../context";
 import { Question } from "@/types/models";
 
@@ -14,6 +14,7 @@ type Props = {};
 export default function QuestionList({}: Props) {
     const { data, setData } = useFormCreateContext();
     const [modeEdit, setModeEdit] = useState(null);
+    const editingRef = useRef<HTMLDivElement>(null);
 
     const onUpdateQuestion = (question) => {
         const index = data.questions.findIndex((q) => q.id === question.id);
@@ -43,16 +44,31 @@ export default function QuestionList({}: Props) {
         setData("questions", newQuestions);
     };
 
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (!editingRef.current?.contains(event.target as Node)) {
+                setModeEdit(null);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [editingRef]);
+
     return (
         <>
             {data.questions.map((question) => (
                 <Container key={question.id}>
                     {modeEdit === question.id ? (
-                        <QuestionEditor
-                            question={question}
-                            onUpdate={onUpdateQuestion}
-                            onDelete={onDeleteQuestion}
-                        />
+                        <div ref={editingRef}>
+                            <QuestionEditor
+                                question={question}
+                                onUpdate={onUpdateQuestion}
+                                onDelete={onDeleteQuestion}
+                            />
+                        </div>
                     ) : (
                         <QuestionPreview
                             question={question}

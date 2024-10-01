8ef3b4a3bbb6303cb3eb267703fd9089ef0a6cfc
rdzPedraos <rdzpedraos@gmail.com>
2024-09-30 19:54:42 -0500

    refactor: remove unused code

--- resources/ts/components/Container.tsx
+++ resources/ts/components/Container.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-export default function Container({ children }: { children: React.ReactNode }) {
+export default function Container({
+    children,
+    startContent,
+}: {
+    children: React.ReactNode;
+    startContent?: React.ReactNode;
+}) {
     return (
-        <div className="relative bg-white shadow border-1 rounded-xl p-5">
-            {children}
+        <div className="relative bg-white shadow border-1 rounded-xl overflow-hidden">
+            {startContent}
+            <div className="p-5">{children}</div>
         </div>
     );
 }

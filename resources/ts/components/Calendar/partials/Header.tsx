70ba1f0d00b7db5889872235fd4d3029eca37589
rdzPedraos <rdzpedraos@gmail.com>
2024-10-03 17:02:03 -0500

    fix: avoid unselect mode in calendar

--- resources/ts/components/Calendar/partials/Header.tsx
+++ resources/ts/components/Calendar/partials/Header.tsx
@@ -78,6 +78,7 @@ export default function Header({}: Props) {
             </div>
 
             <Select
+                disallowEmptySelection
                 className="max-w-32"
                 aria-label="Tipo de vista"
                 variant="flat"

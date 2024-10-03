834c95c6c79961ffa9148f3965045cf58f9416d2
rdzPedraos <rdzpedraos@gmail.com>
2024-10-03 15:10:34 -0500

    feat: add primary key

--- database/migrations/2024_09_24_033503_create_sites_table.php
+++ database/migrations/2024_09_24_033503_create_sites_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('sites', function (Blueprint $table) {
-            $table->unsignedSmallInteger("id", true);
+            $table->unsignedSmallInteger("id", true)->primary();
             $table->string("name", 100);
             $table->string("address", 255);
             $table->softDeletes();

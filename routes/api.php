3789750f63565a6284940aa6bcfcbbc166c34f4c
rdzPedraos <rdzpedraos@gmail.com>
2024-10-05 19:17:08 -0500

    fix: remove ping route because laravel use /up route by default

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\EventController;
 use App\Http\Controllers\SitesController;
 use Illuminate\Support\Facades\Route;
-
-Route::get("/ping", fn() => response()->json(["message" => "pong"]));
 
 Route::get("/sites", [SitesController::class, "getByapi"])->name(name: "api.sites.list");

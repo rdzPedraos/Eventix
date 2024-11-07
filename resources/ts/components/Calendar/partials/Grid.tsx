1cd11860fc7085e5db43d09a581f53e02bc11b9a
rdzPedraos <rdzpedraos@gmail.com>
2024-11-07 18:44:36 -0500

    fix: change way for order elements in grid

--- resources/ts/components/Calendar/partials/Grid.tsx
+++ resources/ts/components/Calendar/partials/Grid.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import Event from "./Event";
 import { hours } from "../utils/calendar";
-import { DayType } from "../utils/types";
+import { DayType, EventType } from "../utils/types";
 
 type Props = {
     day: DayType;
-    events: {
-        id: string;
-        title: string;
-        color: string;
-        startDate: DayType;
-        endDate: DayType;
-    }[];
+    events: EventType[];
 };
+
+const halfHourInPercentage = (0.5 / 24) * 100;
+const hourInPercentage = (1 / 24) * 100;
 
 export default function Grid({ day, events }: Props) {
     const daysEvents = events
@@ -32,19 +29,35 @@ export default function Grid({ day, events }: Props) {
 
         const top = (startHour / 24) * 100;
         let bottom = (endHour / 24) * 100;
-        if (bottom - top < 1) bottom += 1; // Minimum height of 1%
+        if (bottom - top < halfHourInPercentage) bottom = halfHourInPercentage;
 
-        // Calcular la posición horizontal y el ancho del evento
+        // Check if there are events superposed
         const eventosSuperpuestos = eventosRenderizados.filter(
-            ({ position: p }) => top >= p.top && top < p.bottom
+            ({ position: p }) => !(p.bottom <= top || p.top >= bottom)
         );
 
-        const left = eventosSuperpuestos.length * 10; // 10% de margen para cada evento superpuesto
-        const width = 100 - left; // El ancho siempre llega al borde derecho
+        const closerEvent = eventosSuperpuestos.reduce(
+            (prev, current) =>
+                top - current.position.top < top - prev.position.top
+                    ? current
+                    : prev,
+            eventosSuperpuestos[0]
+        );
+
+        let left = 0;
+        let width = 100;
+
+        if (closerEvent && top - closerEvent.position.top < hourInPercentage) {
+            left = closerEvent.position.left + closerEvent.position.width / 2;
+            width = closerEvent.position.width / 2;
+        } else {
+            left = eventosSuperpuestos.length * 5; // 5% de margen para cada evento superpuesto
+            width -= left; // El ancho siempre llega al borde derecho
+        }
 
         //reduce previous events width
         eventosSuperpuestos.forEach((evento) => {
-            evento.position.width -= 10;
+            evento.position.width -= 5;
         });
 
         eventosRenderizados.push({

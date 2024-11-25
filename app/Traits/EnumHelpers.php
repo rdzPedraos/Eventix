8fe900037828560b9543316aaffdb50d70abffee
rdzPedraos <rdzpedraos@gmail.com>
2024-11-24 19:53:43 -0500

    refactor: change code for improve logic

--- app/Traits/EnumHelpers.php
+++ app/Traits/EnumHelpers.php
@@ -6,14 +6,12 @@ trait EnumHelpers
 {
     public static function casesName()
     {
-        $cases = self::cases();
-        return array_map(fn($c) => $c->name, $cases);
+        return array_column(self::cases(), 'name');
     }
 
     public static function casesValue()
     {
-        $cases = self::cases();
-        return array_map(fn($c): mixed => $c->value, $cases);
+        return array_column(self::cases(), 'value');
     }
 
     public static function casesKeyLabel()
